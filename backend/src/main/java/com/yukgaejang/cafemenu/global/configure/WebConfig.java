db2edce5e86e18ee4530433bddf2f3a680c4f71b
songfox00 <songfox00@gmail.com>
2026-08-25 23:17:03 +0900

    edit allowOrigins

--- backend/src/main/java/com/yukgaejang/cafemenu/global/configure/WebConfig.java
+++ backend/src/main/java/com/yukgaejang/cafemenu/global/configure/WebConfig.java
@@ -19,7 +19,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("https://cdpn.io")
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
