2db125fca5e5a8d9269056f43d4169c728e5ed8a
songfox00 <songfox00@gmail.com>
2026-08-28 12:11:23 +0900

    fix: search product api

--- backend/src/main/java/com/yukgaejang/cafemenu/domain/post/product/repository/ProductRepository.java
+++ backend/src/main/java/com/yukgaejang/cafemenu/domain/post/product/repository/ProductRepository.java
@@ -3,13 +3,14 @@ package com.yukgaejang.cafemenu.domain.post.product.repository;
 import com.yukgaejang.cafemenu.domain.post.product.entity.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     boolean existsByName(String name); //이름 검사용
 
-    Page<Product> findAll(Specification<Product> spc, Pageable pageable);
-
+    Page<Product> findDistinctByNameContaining(
+        String productName,
+        Pageable pageable
+    );
 }
